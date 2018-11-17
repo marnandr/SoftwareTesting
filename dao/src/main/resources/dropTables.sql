b4d2dec98d028110698e83c349f6e1d8b46f7800
urgyanmiki <urgyan.miklos98@gmail.com>
2018-11-17 21:14:25 +0100

    I created the basics for the MySQL part of the project.

---
+++ dao/src/main/resources/dropTables.sql
@@ -0,0 +1,8 @@
+DROP TABLE Student_Request_Complain;
+DROP TABLE Student_Request;
+DROP TABLE Courses;
+DROP TABLE Form;
+DROP TABLE Student;
+DROP TABLE Teacher;
+DROP TABLE Administrator;
+DROP TABLE Formtype;

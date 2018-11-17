371918b08c6fe7c69c2a48943910f403b7ad82d7
marnandr <maro@iit.uni-miskolc.hu>
2018-11-17 17:54:40 +0100

    Repair commit. Successful Maven build. The structure restored.

---
+++ core/src/main/java/hu/uni/miskolc/iit/App.java
@@ -0,0 +1,13 @@
+package hu.uni.miskolc.iit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

3a4f7cb54905655cc526afce5b4e9bae02cd48a0
marnandr <maro@iit.uni-miskolc.hu>
2018-11-18 13:31:49 +0100

    Grammar reparation.

--- core/src/main/java/hu/uni/miskolc/iit/service/FormService.java
+++ core/src/main/java/hu/uni/miskolc/iit/service/FormService.java
@@ -9,61 +9,61 @@ public interface FormService {
     /**
      * Method which list the all existing forms.
      * @return
-     * @throws listNotFoundException
+     * @throws ListNotFoundException
      */
-    public Collection<Form> listAll() throws listNotFoundException;
+    public Collection<Form> listAll() throws ListNotFoundException;
 
     /**
      * Method which list all exisiting forms with date filtering.
      * @param date
      * @return
-     * @throws listNotFoundException
-     * @throws notExistingFieldException
+     * @throws ListNotFoundException
+     * @throws NotExistingFieldException
      */
-    public Collection<Form> listByDate(Date date) throws listNotFoundException, notExistingFieldException;
+    public Collection<Form> listByDate(Date date) throws ListNotFoundException, NotExistingFieldException;
 
     /**
      *  Method which list all exisiting forms with state filtering.
      * @param state
      * @return
-     * @throws listNotFoundException
-     * @throws notExistingFieldException
+     * @throws ListNotFoundException
+     * @throws NotExistingFieldException
      */
-    public Collection<Form> listByState(String state) throws listNotFoundException, notExistingFieldException;
+    public Collection<Form> listByState(String state) throws ListNotFoundException, NotExistingFieldException;
 
     /**
      *  Method which list all exisiting forms with course filtering.
      * @param course
      * @return
-     * @throws listNotFoundException
-     * @throws notExistingFieldException
+     * @throws ListNotFoundException
+     * @throws NotExistingFieldException
      */
-    public Collection<Form> listByCourse(String course) throws listNotFoundException, notExistingFieldException;
+    public Collection<Form> listByCourse(String course) throws ListNotFoundException, NotExistingFieldException;
 
     /**
      *  Method which list all exisiting forms with student filtering.
      * @param student
      * @return
-     * @throws listNotFoundException
-     * @throws notExistingFieldException
+     * @throws ListNotFoundException
+     * @throws NotExistingFieldException
      */
-    public Collection<Form> listByStudent(String student) throws listNotFoundException, notExistingFieldException;
+    public Collection<Form> listByStudent(String student) throws ListNotFoundException, NotExistingFieldException;
 
     /**
      *  Method which list all exisiting forms with teacher filtering.
      * @param teacher
      * @return
-     * @throws listNotFoundException
-     * @throws notExistingFieldException
+     * @throws ListNotFoundException
+     * @throws NotExistingFieldException
      */
-    public Collection<Form> listByTeacher(String teacher) throws listNotFoundException, notExistingFieldException;
+    public Collection<Form> listByTeacher(String teacher) throws ListNotFoundException, NotExistingFieldException;
 
     /**
      *  Method which can make decision by a teacher.
      * @param decision
-     * @throws notExistingFieldException
+     * @throws NotExistingFieldException
      */
-    public void makeDecision(boolean decision) throws notExistingFieldException;
+    public void makeDecision(boolean decision) throws NotExistingFieldException;
 
     /**
      *  Method which can make complain by a student.

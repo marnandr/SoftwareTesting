c75f22bf664b2f02f49d5f6ba33db1332a4a0728
Cintia <david.cintia.987@gmail.com>
2018-12-01 17:16:01 +0100

    Implementation of ListForms and createNewRequest.

--- service/src/main/java/hu/uni/miskolc/iit/dao/StudentServiceDao.java
+++ service/src/main/java/hu/uni/miskolc/iit/dao/StudentServiceDao.java
@@ -2,14 +2,22 @@ package hu.uni.miskolc.iit.dao;
 
 import hu.uni.miskolc.iit.model.Course;
 import hu.uni.miskolc.iit.model.Form;
+import hu.uni.miskolc.iit.model.Request;
 
 import java.util.List;
+import javax.sql.DataSource;
 
 public interface StudentServiceDao {
 
     public List<Course> findAllCourses();
 
     public List<Form>   findAllForms();
+
+    public void setDataSource(DataSource ds);
+
+    public Request createRequest(int Request_ID, int Student_ID, int Teacher_ID, String Request_Status, String Request_Description);
+
+    public List<Request> findAllRequests();
 
     public boolean chechkRequestStatus(int requestid, boolean status);
 
--- service/src/main/java/hu/uni/miskolc/iit/service/impl/StudentServiceImpl.java
+++ service/src/main/java/hu/uni/miskolc/iit/service/impl/StudentServiceImpl.java
@@ -2,6 +2,8 @@ package hu.uni.miskolc.iit.service.impl;
 
 import hu.uni.miskolc.iit.dao.StudentServiceDao;
 import hu.uni.miskolc.iit.model.Course;
+import hu.uni.miskolc.iit.model.Form;
+import hu.uni.miskolc.iit.model.Request;
 import hu.uni.miskolc.iit.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,6 +25,12 @@ public class StudentServiceImpl implements StudentService {
     public List<Course> getAllCourses() {
         return studentServiceDao.findAllCourses();
     }
+
+    @Override
+    public List<Form> getAllForms() { return studentServiceDao.findAllForms();}
+
+    @Override
+    public Request createNewRequest(int Request_ID, int Student_ID, int Teacher_ID, String Request_Status, String Request_Description) { return studentServiceDao.createRequest(Request_ID, Student_ID, Teacher_ID, Request_Status, Request_Description);}
 
     @Override
     public boolean checkRequestStatus(int requestid,boolean status) {

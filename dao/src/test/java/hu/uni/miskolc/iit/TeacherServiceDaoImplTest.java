1f0279258f3c277312f15568e58eb30a820a0b36
Anett <r_anett@hotmail.com>
2018-12-02 12:47:18 +0100

    Unit test for the TeacherServiceDao

--- dao/src/test/java/hu/uni/miskolc/iit/TeacherServiceDaoImplTest.java
+++ dao/src/test/java/hu/uni/miskolc/iit/TeacherServiceDaoImplTest.java
@@ -1,21 +1,31 @@
 package hu.uni.miskolc.iit;
 
+import hu.uni.miskolc.iit.dao.TeacherServiceDao;
+import hu.uni.miskolc.iit.model.Form;
 import org.junit.Before;
 import org.junit.Test;
 import hu.uni.miskolc.iit.impl.TeacherServiceDaoImpl;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
 
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class TeacherServiceDaoImplTest {
 
-    TeacherServiceDaoImpl tImp;
+    private TeacherServiceDao teacherServiceDao = new TeacherServiceDaoImpl();
 
-    @Before
-    public void before(){
-        tImp = new TeacherServiceDaoImpl();
+    @Test
+    public void teacherServiceDaoTest() {
+        List<Form> allForms = teacherServiceDao.getForms();
+        assertTrue(allForms.size() > 0);
     }
 
     @Test
     public void getFormsTest(){
-        tImp.getForms();
     }
 }

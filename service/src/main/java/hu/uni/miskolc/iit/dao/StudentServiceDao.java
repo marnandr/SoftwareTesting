eca2f69c7221fb9677a8cac7e9d57a12c698ed6e
Pap Dániel <43150057+PapDani@users.noreply.github.com>
2018-12-10 00:29:45 +0100

    fixes

--- service/src/main/java/hu/uni/miskolc/iit/dao/StudentServiceDao.java
+++ service/src/main/java/hu/uni/miskolc/iit/dao/StudentServiceDao.java
@@ -1,5 +1,6 @@
 package hu.uni.miskolc.iit.dao;
 
+import hu.uni.miskolc.iit.exceptions.ComplainAlreadyExistsException;
 import hu.uni.miskolc.iit.exceptions.RequestDoesNotExistException;
 import hu.uni.miskolc.iit.model.Course;
 import hu.uni.miskolc.iit.model.Form;
@@ -10,9 +11,10 @@ public interface StudentServiceDao {
 
     public List<Course> findAllCourses();
 
-    public List<Form>   findAllForms();
+    public List<Form> findAllForms();
 
-    public boolean checkRequestStatus(int requestid, boolean status) throws RequestDoesNotExistException;
+    public String checkRequestStatus(int requestid) throws RequestDoesNotExistException;
 
-    public boolean createComplain(int complainID, int requestID, String complain);
+    public void createComplain(int requestID, String complain)
+            throws ComplainAlreadyExistsException, IllegalArgumentException, RequestDoesNotExistException;
 }
